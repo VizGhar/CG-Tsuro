8f8cad22da8b5cda6fc59a5db9115f84a1ad634b
Jan Kandrac <jan@kandrac.xyz>
2020-12-21 08:07:44 +0100

    Stub

--- src/main/kotlin/com/codingame/game/turn/GameTurn.kt
+++ src/main/kotlin/com/codingame/game/turn/GameTurn.kt
@@ -80,21 +80,18 @@ fun Referee.movingTurns(playerId: Int) {
 /**
  * These are inputs send to [Player]'s input stream:
  *
- * 1. <opponents> - opponent count
- * 2. <opponents> lines containing
+ * 1. <opponents> lines containing
  *      1. <tileId> - last tile ID played by opponent
  *      2. <tileRotation> - last tile ROTATION played by opponent
  *      3. <col> - column on which opponent is located
  *      4. <row> - row on which opponent is located
  *      5. <index> - position index (0,1 top; 2,3 right...)
- * 3. <cardCount> - count of cards on my hand
- * 4. <cardCount> lines of
+ * 2. <cardCount> - count of cards on my hand
+ * 3. <cardCount> lines of
  *      1. <tileId> - identifier of tile
  *      2. <connections> - 4 pairs of tile indexes to connect
  */
 private fun Referee.sendInputsToPlayer(player: Player) {
-    // opponent count
-    player.sendInputLine((gameManager.players.size - 1).toString())
 
     // last opponent moves
     for (i in 1 until gameManager.players.size) {
